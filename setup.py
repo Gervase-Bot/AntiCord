acb686880e38359f5a5b090f45e8c0d55712dcfe
GervaseBot <127709114+Gervase-Bot@users.noreply.github.com>
2023-03-12 20:12:46 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anticord',
+    version='0.0.1',
+    description='This Library is Used to Make The Most Secure Server Possible. It Contains anti-NSFW, anti-swear, anti-invite, anti-link and everything that can be harmful for communities on discord that they can avoid',
+    author='Gervase',
+    author_email='gervasebot@gmail.com',
+    url='https://github.com/Gervase-Bot/AntiCord',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
